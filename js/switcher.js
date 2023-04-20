c797095192172a9e1719e4f2451d19f2d7f6c44d
Magdalena Holubova <mholubova@proton.me>
2023-04-20 23:55:05 +0200

    switcher new version

--- js/switcher.js
+++ js/switcher.js
@@ -1,15 +1,34 @@
-$("#layout_switch button").on("click", function () {
-    $("#testLayout").removeClass("layout-0").removeClass("layout-1");
-    $("body").addClass("mobile");
-    console.log('mobile layout');
+var switcher = document.querySelector("#layout_switch"),
+    buttons = switcher.querySelectorAll("button"),
+    testLayout = document.querySelector("#testLayout"),
+    body = document.body;
+
+switcher.querySelectorAll("button")[0].addEventListener("click", function () {
+    testLayout.classList.remove("layout-0", "layout-1");
+    body.classList.add("mobile");
+    buttons.forEach(function (button) {
+        button.classList.remove("active")
+    });
+    this.classList.add("active");
+
 });
-$("#layout_switch button:nth-child(2)").on("click", function () {
-    $("#testLayout").addClass("layout-0");
-    $("body.mobile").removeClass("mobile");
-    console.log('3 column layout');
+
+switcher.querySelectorAll("button")[1].addEventListener("click", function () {
+    testLayout.classList.remove("layout-1");
+    body.classList.remove("mobile");
+    testLayout.classList.add("layout-0");
+    buttons.forEach(function (button) {
+        button.classList.remove("active")
+    });
+    this.classList.add("active");
 });
-$("#layout_switch button:nth-child(3)").on("click", function () {
-    $("#testLayout").addClass("layout-1");
-    $("body.mobile").removeClass("mobile");
-    console.log('4 column layout');
+
+switcher.querySelectorAll("button")[2].addEventListener("click", function () {
+    testLayout.classList.remove("layout-0");
+    body.classList.remove("mobile");
+    testLayout.classList.add("layout-1");
+    buttons.forEach(function (button) {
+        button.classList.remove("active")
+    });
+    this.classList.add("active");
 });
